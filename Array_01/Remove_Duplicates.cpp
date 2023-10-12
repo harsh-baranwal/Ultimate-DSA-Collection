ac9480bad53a1979f1c6f608c383d0d71e5cb71d
Harsh Baranwal <harshbaranwalofficial@gmail.com>
2023-10-12 13:35:47 +0530

    Optimal approach

--- Array_01/Remove_Duplicates.cpp
+++ Array_01/Remove_Duplicates.cpp
@@ -14,6 +14,18 @@ int removeDuplicates(int arr[], int n) {
     return k;
 }
 
+// Optimal Approach
+int removeDuplicates(int arr[], int n) {
+    int i = 0;
+    for(int j = 1; j < n; j++) {
+        if(arr[j] != arr[i]) {
+            arr[i+1] = arr[j];
+            i++;
+        }
+    }
+    return i + 1;
+}
+
 int main() {
     int arr[] = {1, 1, 3, 4, 4};
     int n=sizeof(arr)/sizeof(arr[0]);
