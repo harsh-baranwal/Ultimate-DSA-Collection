cbbe233344aee92039b4fa6df900fc8dd035a5e2
Harsh Baranwal <harshbaranwalofficial@gmail.com>
2023-10-16 11:21:22 +0530

    First approach

--- Array_01/Left_Rotate_D_Place.cpp
+++ Array_01/Left_Rotate_D_Place.cpp
@@ -1,16 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute force approach
 vector<int> leftRotate(int arr[], int n, int d) {
     d = d%n;
 
+    // Storing d elements to temp array
     int temp[d];
     for(int i = 0; i < d; i++) {
         temp[i] = arr[i];
     }
+
+    // Skipping elements
     for(int i = d; i < n; i++) {
         arr[i - d] = arr[i];
     }
+
+    // Storing temp elements to array
     for(int i = n-d; i < n; i++) {
         arr[i] = temp[i - (n-d)];
     }
